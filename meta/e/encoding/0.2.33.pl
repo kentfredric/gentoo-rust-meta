dffb1dd7a782e91ea0d8411d77ebcc945619e814
Kent Fredric <kentnl@gentoo.org>
2019-10-27 18:18:05 +1300

    +encoding 0.2.33

---
+++ meta/e/encoding/0.2.33.pl
@@ -0,0 +1,14 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    encoding => '0.2.33',
+    requires => {
+        'encoding-index-singlebyte'  => '~1.20141219.5',
+        'encoding-index-korean'      => '~1.20141219.5',
+        'encoding-index-japanese'    => '~1.20141219.5',
+        'encoding-index-simpchinese' => '~1.20141219.5',
+        'encoding-index-tradchinese' => '~1.20141219.5',
+    }
+);
