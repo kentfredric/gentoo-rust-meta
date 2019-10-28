38673cfb09cd580c7ebedf4ae6bdb5110ca5981b
Kent Fredric <kentnl@gentoo.org>
2019-10-29 00:04:05 +1300

    +unicode-xid 0.2.0

---
+++ meta/u/unicode-xid/0.2.0.pl
@@ -0,0 +1,12 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    'unicode-xid' => '0.2.0',
+    features      => {
+        bench   => [],
+        default => [],
+        no_std  => [],
+    }
+);
