be1f8fae1e4aa6160ba5384dbe4363989ae4a892
Kent Fredric <kentnl@gentoo.org>
2019-10-28 21:03:24 +1300

    +url 1.7.2

---
+++ meta/u/url/1.7.2.pl
@@ -0,0 +1,32 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    # TODO
+    url           => '1.7.2',
+    missing_tests => 1,
+    optional      => {
+        encoding          => '^0.2',
+        heapsize          => '>=0.4.1, <0.5',
+        'rustc-serialize' => '^0.3',
+        serde             => '>=0.6.1, <0.9',
+    },
+    requires => {
+        idna               => '^0.1.0',
+        matches            => '^0.1',
+        'percent-encoding' => '^1',
+    },
+    test => {
+
+        # benches/ only
+        # bencher => '^0.1',
+        'rustc-test' => '^0.3',
+        serde_json   => '>=0.6.1, <0.9',
+    },
+    features => {
+        heap_size      => [qw( heapsize )],
+        query_encoding => [qw( encoding )]
+      }
+
+);
