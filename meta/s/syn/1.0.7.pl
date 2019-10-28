13d9d43bd539b728f47f20845ecb326516b68c5f
Kent Fredric <kentnl@gentoo.org>
2019-10-28 20:02:38 +1300

    +syn 1.0.7

---
+++ meta/s/syn/1.0.7.pl
@@ -0,0 +1,38 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    syn => '1.0.7',
+
+    requires => {
+        'proc-macro2' => '^1',
+        'unicode-xid' => '^0.2',
+    },
+    optional => {
+        quote => '^1',
+    },
+    test => {
+        insta      => '^0.11',
+        rayon      => '^1.0',
+        'ref-cast' => '^0.2',
+        regex      => '^1',
+        termcolor  => '^1',
+        walkdir    => '^2.1',
+    },
+    features => {
+        'clone-impls' => [],
+        default =>
+          [ 'derive', 'parsing', 'printing', 'clone-impls', 'proc-macro' ],
+        derive         => [],
+        'extra-traits' => [],
+        fold           => [],
+        full           => [],
+        parsing        => [],
+        printing       => ['quote'],
+        'proc-macro'   => [ 'proc-macro2/proc-macro', 'quote/proc-macro' ],
+        visit          => [],
+        'visit-mut'    => [],
+    }
+);
+
