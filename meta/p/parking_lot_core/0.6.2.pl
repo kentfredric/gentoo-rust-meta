554a99f2ef5b6d855b4e6159ce12910c00e83b0b
Kent Fredric <kentnl@gentoo.org>
2019-10-30 01:27:54 +1300

    +parking_lot_core 0.6.2

---
+++ meta/p/parking_lot_core/0.6.2.pl
@@ -0,0 +1,36 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    # TODO
+    # NOTE: Tests restricted, features missing
+    parking_lot_core => '0.6.2',
+    missing_tests    => 1,
+    missing_options  => 1,
+    requires         => {
+        'cfg-if' => '^0.1.5',
+        smallvec => '^0.6',
+
+        # cloudabi
+        # cloudabi => '^0.0.3',
+        # redox
+        # redox_syscall => '^0.1',
+        # unix
+        libc => '^0.2.55',
+
+        # windows
+        # winapi => '^0.3',
+    },
+    build_requires => {
+        rustc_version => '^0.2'
+    },
+    optional => {
+        backtrace   => '^0.3.2',
+        petgraph    => '^0.4.5',
+        'thread-id' => '^3.2.0',
+    },
+    features => {
+        deadlock_detection => [ 'petgraph', 'thread-id', 'backtrace' ],
+    }
+);
