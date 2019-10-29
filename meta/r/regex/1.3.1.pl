caa0212323e04675f6f1424ffaa9c67a4dc89a24
Kent Fredric <kentnl@gentoo.org>
2019-10-30 01:40:17 +1300

    +regex 1.3.1

---
+++ meta/r/regex/1.3.1.pl
@@ -0,0 +1,44 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    # TODO
+    # NOTE: Tests restricted, features missing
+    regex           => '1.3.1',
+    missing_tests   => 1,
+    missing_options => 1,
+    requires        => {
+        'regex-syntax' => '^0.6.12',
+    },
+    optional => {
+        'aho-corasick' => '^0.7.6',
+        memchr         => '^2.2.1',
+        thread_local   => '^0.3.6',
+    },
+    test => {
+        'doc-comment' => '^0.3',
+        lazy_static   => '^1',
+        quickcheck    => '^0.8',
+        rand          => '^0.6.5',
+    },
+    features => {
+        default => [ 'std', 'perf', 'unicode' ],
+        pattern => [],
+        perf    => [ 'perf-cache', 'perf-dfa', 'perf-inline', 'perf-literal' ],
+        'perf-cache'   => ['thread_local'],
+        'perf-dfa'     => [],
+        'perf-inline'  => [],
+        'perf-literal' => [ 'aho-corasick', 'memchr' ],
+        std            => [],
+        unicode        => [
+            map { "unicode-$_" } qw( age bool case gencat perl script segment )
+        ],
+        (
+            map { ( "unicode-$_", ["regex-syntax/unicode-$_"] ) }
+              qw( age bool case gencat perl script segment )
+        ),
+        unstable => ['pattern'],
+        use_std  => ['std'],
+    }
+);
