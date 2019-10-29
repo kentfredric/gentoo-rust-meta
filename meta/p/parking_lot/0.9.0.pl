b880e56ec468f2bffeb100b917f8cf4ee61ff5f4
Kent Fredric <kentnl@gentoo.org>
2019-10-30 01:32:57 +1300

    +parking_lot 0.9.0

---
+++ meta/p/parking_lot/0.9.0.pl
@@ -0,0 +1,30 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    # TODO
+    # NOTE: Tests restricted, features missing
+    parking_lot     => '0.9.0',
+    missing_tests   => 1,
+    missing_options => 1,
+    requires        => {
+        lock_api         => '^0.3.1',
+        parking_lot_core => '^0.6',
+    },
+    build_requires => {
+        rustc_version => '^0.2',
+    },
+    test => {
+        bincode     => '^1.1.3',
+        lazy_static => '^1',
+        rand        => '^0.7',
+    },
+    features => {
+        deadlock_detection => ['parking_lot_core/deadlock_detection'],
+        default            => [],
+        nightly    => [ 'parking_lot_core/nightly', 'lock_api/nightly' ],
+        owning_ref => ['lock_api/owning_ref'],
+        serde      => ['lock_api/serde'],
+    }
+);
