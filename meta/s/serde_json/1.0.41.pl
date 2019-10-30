9e15f81419594b32163a9cf4d9b2bebc7f9b0ad3
Kent Fredric <kentnl@gentoo.org>
2019-10-31 10:55:51 +1300

    serde_json 1.0.41 enable tests and features

--- meta/s/serde_json/1.0.41.pl
+++ meta/s/serde_json/1.0.41.pl
@@ -3,12 +3,8 @@ use strict;
 use warnings;
 
 crate(
-    # TODO
-    # NOTE: Tests Restricted, Missing feaures
-    serde_json      => '1.0.41',
-    missing_tests   => 1,
-    missing_options => 1,
-    requires        => {
+    serde_json => '1.0.41',
+    requires   => {
         itoa  => '^0.4.3',
         ryu   => '^1',
         serde => '^1.0.60',
