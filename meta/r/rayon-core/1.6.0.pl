7c21e89fd1e5d14ba146923292fc61a42c6d8dd8
Kent Fredric <kentnl@gentoo.org>
2019-10-31 03:18:38 +1300

    +rayon-core 1.6.0

---
+++ meta/r/rayon-core/1.6.0.pl
@@ -0,0 +1,23 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    # TODO
+    # NOTE: Tests restricted
+    'rayon-core'  => '1.6.0',
+    missing_tests => 1,
+    requires      => {
+        'crossbeam-deque' => '^0.7',
+        'crossbeam-queue' => '^0.1.2',
+        'crossbeam-utils' => '^0.6.5',
+        lazy_static       => '^1',
+        num_cpus          => '^1.2',
+    },
+    test => {
+        rand          => '^0.6',
+        rand_xorshift => '^0.1',
+        'scoped-tls'  => '^1',
+        libc          => '^0.2',
+    }
+);
