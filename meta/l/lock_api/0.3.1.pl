f762ebd8da36e5956c11d017287bc56eb77a3de8
Kent Fredric <kentnl@gentoo.org>
2019-10-31 11:30:56 +1300

    lock_api 0.3.1 enable tests and features

--- meta/l/lock_api/0.3.1.pl
+++ meta/l/lock_api/0.3.1.pl
@@ -3,12 +3,8 @@ use strict;
 use warnings;
 
 crate(
-    # TODO
-    # NOTE: Tests restricted, features missing
-    lock_api        => '0.3.1',
-    missing_tests   => 1,
-    missing_options => 1,
-    optional        => {
+    lock_api => '0.3.1',
+    optional => {
         'owning_ref' => '^0.4',
         scopeguard   => '^1',
         serde        => '^1.0.90',
