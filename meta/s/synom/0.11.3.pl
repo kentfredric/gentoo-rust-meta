b089ad26b1ff0a5964e9ad97f0d9aee81fccc3c6
Kent Fredric <kentnl@gentoo.org>
2019-10-31 12:05:12 +1300

    synom 0.11.3 enable tests

--- meta/s/synom/0.11.3.pl
+++ meta/s/synom/0.11.3.pl
@@ -3,9 +3,8 @@ use strict;
 use warnings;
 
 crate(
-    synom         => '0.11.3',
-    missing_tests => 1,
-    requires      => {
+    synom    => '0.11.3',
+    requires => {
         'unicode-xid' => '^0.0.4',
     },
     test => {
