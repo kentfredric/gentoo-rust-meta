92ff9261f73f5f87678a84931065b38ad0105271
Kent Fredric <kentnl@gentoo.org>
2019-10-31 02:56:54 +1300

    +serde_yaml 0.8.11

---
+++ meta/s/serde_yaml/0.8.11.pl
@@ -0,0 +1,20 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    # TODO
+    serde_yaml      => '0.8.11',
+    missing_tests   => 1,
+    missing_options => 1,
+    requires        => {
+        dtoa              => '^0.4',
+        'linked-hash-map' => '^0.5',
+        serde             => '^1.0.60',
+        'yaml-rust'       => '^0.4',
+    },
+    test => {
+        serde_derive => '^1',
+        unindent     => '^0.1',
+    }
+);
