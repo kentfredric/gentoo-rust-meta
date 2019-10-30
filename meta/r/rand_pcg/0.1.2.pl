6609b26638838e55f9f47871bc18f5533be6467d
Kent Fredric <kentnl@gentoo.org>
2019-10-31 03:01:55 +1300

    +rand_pcg 0.1.2

---
+++ meta/r/rand_pcg/0.1.2.pl
@@ -0,0 +1,27 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    # TODO
+    # NOTE: Tests Restricted
+    rand_pcg      => '0.1.2',
+    missing_tests => 1,
+    requires      => {
+        rand_core => '^0.4',
+    },
+    optional => {
+        serde        => '^1',
+        serde_derive => '^1.0.38',
+    },
+    test => {
+        bincode => '^1.1.2',
+    },
+    build_requires => {
+        autocfg => '^0.1',
+    },
+    features => {
+        serde1 => [qw( serde serde_derive )]
+    },
+
+);
