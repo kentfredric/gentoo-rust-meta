48e7bb83aba2060d73f82f62d7c001c75ce7db4a
Kent Fredric <kentnl@gentoo.org>
2019-11-09 21:28:25 +1300

    +rand 0.7.2

---
+++ meta/r/rand/0.7.2.pl
@@ -0,0 +1,56 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    # TODO
+    rand            => '0.7.2',
+    missing_tests   => 1,
+    missing_options => 1,
+    optional        => {
+        getrandom => '^0.1.1',
+        log       => '^0.4.4',
+
+        # nigthly
+        # packed_simd => '^0.3',
+        rand_pcg => '^0.2',
+
+    },
+    requires => {
+        rand_core => "^0.5.1",
+
+        # not emscripten
+        rand_chacha => '^0.2.1',
+
+        # emscripten
+        # rand_hc => '^0.2.22',
+
+        # unix
+        libc => '^0.2.22',
+    },
+    test => {
+        rand_hc       => '^0.2',
+        rand_isaac    => '^0.2',
+        rand_pcg      => '^0.2',
+        rand_xorshift => '^0.2',
+        rand_xoshiro  => '^0.4',
+    },
+    features => {
+        alloc       => [qw( rand_core/alloc )],
+        default     => [qw( std )],
+        f_getrandom => [qw( getrandom rand_core/getrandom )],
+
+        # Nightly
+        # nightly => [qw( simd_support )],
+        serde1 => [],
+
+        # Nightly
+        # simd_support => [qw( packed_simd )],
+        small_rng => [qw( rand_pcg )],
+        std       => [qw( rand_core/std rand_chacha/std alloc f_getrandom )],
+
+        # wasm
+        #stdweb => [qw( getrandom/stdweb )],
+        #'wasm-bindgen' => [qw( getrandom/wasm-bindgen )],
+    }
+);
