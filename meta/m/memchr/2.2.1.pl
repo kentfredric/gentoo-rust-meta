0821c2a5d526bdcfa5d29210570f3f73bcfaf006
Kent Fredric <kentnl@gentoo.org>
2019-11-12 05:41:33 +1300

    memchr 2.2.1 enable tests

--- meta/m/memchr/2.2.1.pl
+++ meta/m/memchr/2.2.1.pl
@@ -3,11 +3,8 @@ use strict;
 use warnings;
 
 crate(
-    # TODO
-    # NOTE: Tests restricted, features missing
-    memchr        => '2.2.1',
-    missing_tests => 1,
-    optional      => {
+    memchr   => '2.2.1',
+    optional => {
         libc => '^0.2.18',
     },
     test => {
