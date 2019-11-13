803254a5fcf213e5a8ef5e6f7a903999a90a88df
Kent Fredric <kentnl@gentoo.org>
2019-11-14 01:38:35 +1300

    cc 1.0.46 all dependency aspects of features/tests done

--- meta/c/cc/1.0.46.pl
+++ meta/c/cc/1.0.46.pl
@@ -3,11 +3,8 @@ use strict;
 use warnings;
 
 crate(
-    # TODO
-    cc              => '1.0.46',
-    missing_options => 1,
-    missing_tests   => 1,
-    optional        => {
+    cc       => '1.0.46',
+    optional => {
         jobserver => '^0.1.16',
         num_cpus  => '^1.10',
     },
