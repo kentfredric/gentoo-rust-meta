8addb446a4eda80a85f4c4d45c090ddf21ebe3bc
Kent Fredric <kentnl@gentoo.org>
2019-11-14 03:19:05 +1300

    +cpp_demangle 0.2.13

---
+++ meta/c/cpp_demangle/0.2.13.pl
@@ -0,0 +1,34 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    cpp_demangle    => '0.2.13',
+    missing_tests   => 1,
+    missing_options => 1,
+    optional        => {
+        afl => '^0.4.4',
+    },
+    requires => {
+        'cfg-if' => '^0.1.9',
+    },
+    test => {
+
+        # clap => '^2.33.0', # example
+        diff => '^0.1.11',
+    },
+    build_requires => {
+        glob => '^0.3.0',
+    },
+    features => {
+        alloc               => [],
+        cppfilt             => [],
+        default             => [qw( std )],
+        fuzz                => [qw( afl )],
+        logging             => [],
+        nightly             => [],
+        run_libiberty_tests => [],
+        std                 => [],
+    },
+);
+
