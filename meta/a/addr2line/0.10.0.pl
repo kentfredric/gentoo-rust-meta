78194f3ef28006c0d9249eb98873e0d76fe7296b
Kent Fredric <kentnl@gentoo.org>
2019-11-19 12:54:29 +1300

    +addr2line 0.10.0

---
+++ meta/a/addr2line/0.10.0.pl
@@ -0,0 +1,36 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    addr2line       => '0.10.0',
+    missing_tests   => 1,
+    missing_options => 1,
+    optional        => {
+        cpp_demangle     => '^0.2',
+        object           => '^0.12',
+        'rustc-demangle' => '^0.1',
+    },
+    requires => {
+        'fallible-iterator' => '^0.2',
+        gimli               => '^0.19',
+        intervaltree        => '^0.2',
+        lazycell            => '^1.0',
+        smallvec            => '^0.6',
+    },
+    test => {
+        backtrace => '^0.3.13',
+
+        # clap         => '^2', # example
+        findshlibs   => '^0.5',
+        memmap       => '^0.7',
+        'rustc-test' => '^0.3',
+    },
+    features => {
+        alloc        => [qw( gimli/alloc )],
+        default      => [qw( rustc-demangle cpp_demangle std-object )],
+        std          => [qw( gimli/std intervaltree/std smallvec/std )],
+        'std-object' => [qw( std object object/std )],
+    }
+);
+
