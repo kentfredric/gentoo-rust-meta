e3227905897c7a3e96492fb4f6027765945815ea
Kent Fredric <kentnl@gentoo.org>
2019-11-19 18:15:46 +1300

    byteorder 1.3.2 enable tests

--- meta/b/byteorder/1.3.2.pl
+++ meta/b/byteorder/1.3.2.pl
@@ -3,11 +3,8 @@ use strict;
 use warnings;
 
 crate(
-    # TODO
-    # NOTE: Tests restricted
-    byteorder     => '1.3.2',
-    missing_tests => 1,
-    test          => {
+    byteorder => '1.3.2',
+    test      => {
         'doc-comment' => '^0.3',
         quickcheck    => '^0.8',
         rand          => '^0.6',
