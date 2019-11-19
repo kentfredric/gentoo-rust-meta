ffeffb87c3e4f95ed47e31dc30449156475fa36a
Kent Fredric <kentnl@gentoo.org>
2019-11-19 18:52:03 +1300

    digest 0.8.1 enable tests

--- meta/d/digest/0.8.1.pl
+++ meta/d/digest/0.8.1.pl
@@ -3,12 +3,8 @@ use strict;
 use warnings;
 
 crate(
-    # TODO
-    # NOTE: Tests restricted, features missing
-    digest          => '0.8.1',
-    missing_tests   => 1,
-    missing_options => 1,
-    requires        => {
+    digest   => '0.8.1',
+    requires => {
         'generic-array' => '^0.12',
     },
     optional => {
