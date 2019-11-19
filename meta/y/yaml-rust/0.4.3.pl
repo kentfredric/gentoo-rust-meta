3cdfcca30272da433dbfaff5fc7042b93d40366c
Kent Fredric <kentnl@gentoo.org>
2019-11-20 07:11:56 +1300

    yaml-rust 0.4.3 enable tests

--- meta/y/yaml-rust/0.4.3.pl
+++ meta/y/yaml-rust/0.4.3.pl
@@ -3,10 +3,8 @@ use strict;
 use warnings;
 
 crate(
-    # TODO
-    'yaml-rust'   => '0.4.3',
-    missing_tests => 1,
-    requires      => {
+    'yaml-rust' => '0.4.3',
+    requires    => {
         'linked-hash-map' => '>0.0.9, <=0.6',
     },
     test => {
