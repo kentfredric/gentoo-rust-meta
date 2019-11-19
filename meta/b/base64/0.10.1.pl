00d329b305986ce8aacb8e457285963f9cfbe8aa
Kent Fredric <kentnl@gentoo.org>
2019-11-19 21:25:19 +1300

    base64 0.10.1 enable tests

--- meta/b/base64/0.10.1.pl
+++ meta/b/base64/0.10.1.pl
@@ -3,15 +3,13 @@ use strict;
 use warnings;
 
 crate(
-    # TODO
-    # NOTE: Tests restricted
-    base64        => '0.10.1',
-    missing_tests => 1,
-    requires      => {
+    base64   => '0.10.1',
+    requires => {
         byteorder => '^1.2.6',
     },
     test => {
-        criterion => '^0.2',
-        rand      => '^0.6.1',
+
+        # criterion => '^0.2', # benches
+        rand => '^0.6.1',
     }
 );
