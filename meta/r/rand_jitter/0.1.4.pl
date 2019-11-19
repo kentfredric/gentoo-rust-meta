d645816e6fa050292c7935f9d09105669e2a4f7d
Kent Fredric <kentnl@gentoo.org>
2019-11-20 10:08:38 +1300

    rand_jitter 0.1.4 enable tests & features

--- meta/r/rand_jitter/0.1.4.pl
+++ meta/r/rand_jitter/0.1.4.pl
@@ -3,12 +3,8 @@ use strict;
 use warnings;
 
 crate(
-    # TODO
-    # NOTE: Tests restricted, features missing
-    rand_jitter     => '0.1.4',
-    missing_tests   => 1,
-    missing_options => 1,
-    optional        => {
+    rand_jitter => '0.1.4',
+    optional    => {
         log => '^0.4',
     },
     requires => {
