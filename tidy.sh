0b3e7be69db78047a86de82e158269917382c215
Kent Fredric <kentnl@gentoo.org>
2019-11-19 14:58:39 +1300

    Enable avoiding invoking perltidy for speed

--- tidy.sh
+++ tidy.sh
@@ -3,6 +3,7 @@ shopt -q -u dotglob
 shopt -q -s globstar
 shopt -q -s nullglob
 
-perltidy -b -bext=/ meta/**/*.pl rdeps.pl lib/*.pm  || exit 1
-
+if [ "${NOTIDY:-0}" == "0" ]; then
+  perltidy -b -bext=/ meta/**/*.pl rdeps.pl lib/*.pm  || exit 1
+fi
 perl rdeps.pl > deps.tgf || exit 1
