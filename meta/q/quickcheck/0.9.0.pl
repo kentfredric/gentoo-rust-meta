55f8fb55af036275054bb818a511160458ec7a1d
Kent Fredric <kentnl@gentoo.org>
2019-11-27 11:01:56 +1300

    +quickcheck 0.9.0

---
+++ meta/q/quickcheck/0.9.0.pl
@@ -0,0 +1,21 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    quickcheck => '0.9.0',
+    optional   => {
+        env_logger => '^0.6',
+        log        => '^0.4',
+    },
+    requires => {
+        rand      => '^0.7',
+        rand_core => '^0.5',
+    },
+    features => {
+        default     => [qw( regex use_logging )],
+        regex       => [qw( env_logger/regex )],
+        unstable    => [],
+        use_logging => [qw( log env_logger )],
+    }
+);
