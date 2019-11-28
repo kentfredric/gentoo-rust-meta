69ea3705117b73e6e4e7f320ae0c80008e179339
Kent Fredric <kentnl@gentoo.org>
2019-11-28 22:36:52 +1300

    +proc-macro-hack 0.5.11

---
+++ meta/p/proc-macro-hack/0.5.11.pl
@@ -0,0 +1,18 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    'proc-macro-hack' => '0.5.11',
+    missing_tests     => 1,
+    requires          => {
+        'proc-macro2' => '^1',
+        quote         => '^1',
+        syn           => '^1.0.5',
+    },
+    test => {
+        'demo-hack'      => '^0.0.5',
+        'demo-hack-impl' => '^0.0.5',
+    },
+);
+
