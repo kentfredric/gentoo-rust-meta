a35ccb42fdee6c1eef2ee74ef5cff71e4f227f1d
Kent Fredric <kentnl@gentoo.org>
2019-12-01 23:38:13 +1300

    unicode-bidi 0.3.4 enable tests

--- meta/u/unicode-bidi/0.3.4.pl
+++ meta/u/unicode-bidi/0.3.4.pl
@@ -3,15 +3,13 @@ use strict;
 use warnings;
 
 crate(
-    # TODO
-    # NOTE: Tests restricted, some features missing
-    'unicode-bidi'  => '0.3.4',
-    missing_tests   => 1,
-    missing_options => 1,
-    optional        => {
-        flame  => '^0.1',
-        flamer => '^0.1',
-        serde  => '>=0.8, <2.0',
+    'unicode-bidi' => '0.3.4',
+    optional       => {
+
+        # not usable on recent rust, not really used in code usefully
+        # flame  => '^0.1',
+        #  flamer => '^0.1',
+        serde => '>=0.8, <2.0',
     },
     requires => {
         matches => '^0.1',
@@ -21,9 +19,11 @@ crate(
     },
     features => {
         with_serde => [qw( serde )],
-        flame_it   => [qw( flame flamer )],
-        unstable   => [],
-        default    => [],
-        bench_it   => [],
+
+        # See above
+        #    flame_it   => [qw( flame flamer )],
+        unstable => [],
+        default  => [],
+        bench_it => [],
     }
 );
