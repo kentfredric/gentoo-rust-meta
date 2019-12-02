e3da87bba3e66121f0459a4ebfa01452013e60c3
Kent Fredric <kentnl@gentoo.org>
2019-12-02 19:10:12 +1300

    +term 0.4.6

---
+++ meta/t/term/0.4.6.pl
@@ -0,0 +1,12 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    term     => '0.4.6',
+    requires => {
+
+        # winapi         => '^0.2', # windows
+        # 'kernel32-sys' => '^0.2', # windows
+    }
+);
