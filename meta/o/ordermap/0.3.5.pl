6fc2fa500caf913239ba27670359d6f2f0d0b10c
Kent Fredric <kentnl@gentoo.org>
2019-12-03 05:04:14 +1300

    ordermap 0.3.5 enable tests

--- meta/o/ordermap/0.3.5.pl
+++ meta/o/ordermap/0.3.5.pl
@@ -3,14 +3,13 @@ use strict;
 use warnings;
 
 crate(
-    ordermap      => '0.3.5',
-    missing_tests => 1,
-    optional      => {
+    ordermap => '0.3.5',
+    optional => {
         serde => '^1',
     },
     test => {
         fnv        => '^1',
-        itertools  => '^1',
+        itertools  => '^0.7.0',
         quickcheck => '^0.6',
         serde_test => '^1.0.5',
     },
