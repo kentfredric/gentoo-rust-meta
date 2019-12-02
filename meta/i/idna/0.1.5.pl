33655c77e7fc24083ef9ff162a62c04fc3ac3ad5
Kent Fredric <kentnl@gentoo.org>
2019-12-02 19:50:09 +1300

    idna 0.1.5 enable tests

--- meta/i/idna/0.1.5.pl
+++ meta/i/idna/0.1.5.pl
@@ -3,11 +3,8 @@ use strict;
 use warnings;
 
 crate(
-    # TODO
-    # NOTE: Tests restricted
-    idna          => '0.1.5',
-    missing_tests => 1,
-    requires      => {
+    idna     => '0.1.5',
+    requires => {
         matches                 => '^0.1',
         'unicode-bidi'          => '^0.3',
         'unicode-normalization' => '^0.1.5',
