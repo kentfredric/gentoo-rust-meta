5f99171e96df98b1bbff34d78d696ce1673649a2
Kent Fredric <kentnl@gentoo.org>
2019-12-03 03:37:30 +1300

    +quickcheck 0.6.2

---
+++ meta/q/quickcheck/0.6.2.pl
@@ -0,0 +1,21 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    quickcheck => '0.6.2',
+    optional   => {
+        env_logger => '^0.5',
+        log        => '^0.4',
+    },
+    requires => {
+        rand => '^0.4',
+    },
+    features => {
+        default     => [qw( regex use_logging )],
+        i128        => [qw( rand/i128_support )],
+        regex       => [qw( env_logger/regex )],
+        unstable    => [],
+        use_logging => [qw( log env_logger )],
+    }
+);
