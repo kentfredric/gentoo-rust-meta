9cf1ef6546ec59d9dd2508f175842a7fe52b76c3
Kent Fredric <kentnl@gentoo.org>
2019-12-04 06:58:01 +1300

    regex-syntax 0.3.9 enable tests

--- meta/r/regex-syntax/0.3.9.pl
+++ meta/r/regex-syntax/0.3.9.pl
@@ -3,10 +3,7 @@ use strict;
 use warnings;
 
 crate(
-    # TODO
-    # NOTE: Tests Restricted
     'regex-syntax' => '0.3.9',
-    missing_tests  => 1,
     test           => {
         quickcheck => '^0.2',
         rand       => '^0.3',
