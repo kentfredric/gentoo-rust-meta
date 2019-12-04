b4bf49ccaa4f860c0684c5bf656a316a9d3e868b
Kent Fredric <kentnl@gentoo.org>
2019-12-04 18:21:09 +1300

    utf8-ranges 0.1.3 enable tests

--- meta/u/utf8-ranges/0.1.3.pl
+++ meta/u/utf8-ranges/0.1.3.pl
@@ -3,10 +3,7 @@ use strict;
 use warnings;
 
 crate(
-    # TODO
-    # NOTE: Tests Restricted
     'utf8-ranges' => '0.1.3',
-    missing_tests => 1,
     test          => {
         quickcheck => '^0.2',
     }
