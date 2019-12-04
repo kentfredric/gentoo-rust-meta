b1519024189fcb59463fee325225fe7ddc1bd578
Kent Fredric <kentnl@gentoo.org>
2019-12-04 21:04:52 +1300

    regex 0.1.80 enable tests

--- meta/r/regex/0.1.80.pl
+++ meta/r/regex/0.1.80.pl
@@ -3,10 +3,8 @@ use strict;
 use warnings;
 
 crate(
-    regex           => '0.1.80',
-    missing_tests   => 1,
-    missing_options => 1,
-    requires        => {
+    regex    => '0.1.80',
+    requires => {
         'aho-corasick' => '^0.5.3',
         memchr         => '^0.1.9',
         thread_local   => '^0.2.4',
@@ -14,7 +12,8 @@ crate(
         'utf8-ranges'  => '^0.1.3',
     },
     optional => {
-        simd => '0.1.0',
+
+        #  simd => '0.1.0', # nightly
     },
     test => {
         lazy_static => '^0.1',
@@ -22,7 +21,8 @@ crate(
         rand        => '^0.3',
     },
     features => {
-        pattern      => [],
-        'simd-accel' => [qw( simd )],
+        pattern => [],
+
+        # 'simd-accel' => [qw( simd )], # nightly
     }
 );
