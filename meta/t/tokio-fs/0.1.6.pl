c036bfc5d10323344c16733c560f15422d39de0a
Kent Fredric <kentnl@gentoo.org>
2019-12-05 17:50:06 +1300

    +tokio-fs 0.1.6

---
+++ meta/t/tokio-fs/0.1.6.pl
@@ -0,0 +1,20 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    'tokio-fs'    => '0.1.6',
+    missing_tests => 1,
+    requires      => {
+        futures            => '^0.1.21',
+        'tokio-io'         => '^0.1.6',
+        'tokio-threadpool' => '^0.1.3',
+    },
+    test => {
+        rand       => '^0.6',
+        tempdir    => '^0.3',
+        tempfile   => '^3',
+        tokio      => '^0.1.7',
+        'tokio-io' => '^0.1.6',
+    }
+);
