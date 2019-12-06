7057f0d445218dc4cf6aaac6a01edaeb7e0bf3e1
Kent Fredric <kentnl@gentoo.org>
2019-12-07 07:28:03 +1300

    +tokio-uds 0.2.5

---
+++ meta/t/tokio-uds/0.2.5.pl
@@ -0,0 +1,24 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    'tokio-uds'   => '0.2.5',
+    missing_tests => 1,
+    requires      => {
+        bytes           => '^0.4.8',
+        futures         => '^0.1.21',
+        iovec           => '^0.1.2',
+        libc            => '^0.2.42',
+        log             => '^0.4.2',
+        mio             => '^0.6.14',
+        'mio-uds'       => '^0.6.5',
+        'tokio-codec'   => '^0.1.0',
+        'tokio-io'      => '^0.1.6',
+        'tokio-reactor' => '^0.1.1',
+    },
+    test => {
+        tempfile => '^3',
+        tokio    => '^0.1.6',
+    },
+);
