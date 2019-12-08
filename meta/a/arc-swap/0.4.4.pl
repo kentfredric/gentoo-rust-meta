0be700992e0f491e73073f4d7a886ca3e010187b
Kent Fredric <kentnl@gentoo.org>
2019-12-08 19:41:19 +1300

    +arc-swap 0.4.4

---
+++ meta/a/arc-swap/0.4.4.pl
@@ -0,0 +1,20 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    'arc-swap'    => '0.4.4',
+    missing_tests => 1,
+    test          => {
+        'crossbeam-utils' => '~0.6',
+        itertools         => '~0.8',
+        model             => '~0.1',
+        num_cpus          => '~1',
+        once_cell         => '~1',
+        proptest          => '~0.8',
+        'version-sync'    => '~0.8',
+    },
+    features => {
+        'unstable-weak' => [qw( )],
+    }
+);
