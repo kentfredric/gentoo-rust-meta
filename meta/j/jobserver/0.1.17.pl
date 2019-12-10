e46ba4d097cc30b512e6178651078bd51f2536ad
Kent Fredric <kentnl@gentoo.org>
2019-12-10 22:01:19 +1300

    jobserver 0.1.17 enable tests

--- meta/j/jobserver/0.1.17.pl
+++ meta/j/jobserver/0.1.17.pl
@@ -3,12 +3,13 @@ use strict;
 use warnings;
 
 crate(
-    # TODO
-    # NOTE: Tests Restricted
-    jobserver     => '0.1.17',
-    missing_tests => 1,
-    requires      => {
+    jobserver => '0.1.17',
+    requires  => {
         log => '^0.4.6',
+
+        # unix
+        libc => '^0.2.50',
+
     },
     test => {
         futures         => '^0.1',
@@ -16,9 +17,6 @@ crate(
         tempdir         => '^0.3',
         'tokio-core'    => '^0.1',
         'tokio-process' => '^0.2',
-
-        # unix
-        libc => '^0.2.50',
 
         # windows
         # getrandom => '^0.1.12',
