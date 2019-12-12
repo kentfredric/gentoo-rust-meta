9e6397c2ee71284ca7ef7b940979a68686009353
Kent Fredric <kentnl@gentoo.org>
2019-12-13 03:12:34 +1300

    +lazy_static 1.1.0

---
+++ meta/l/lazy_static/1.1.0.pl
@@ -0,0 +1,17 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    lazy_static => '1.1.0',
+    optional    => {
+        spin => '^0.4.6',
+    },
+    build_requires => {
+        version_check => '^0.1.4',
+    },
+    features => {
+        nightly     => [],
+        spin_no_std => [qw( nightly spin )],
+    },
+);
