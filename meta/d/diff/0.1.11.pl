bab645e44b522115cd365c32012e6ecd9b160c3c
Kent Fredric <kentnl@gentoo.org>
2019-12-18 02:02:29 +1300

    +diff 0.1.11

---
+++ meta/d/diff/0.1.11.pl
@@ -0,0 +1,13 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    diff => '0.1.11',
+    test => {
+
+        # patched out
+        # quickcheck => '0.4.1',
+        # speculate => '0.0.25',
+    }
+);
