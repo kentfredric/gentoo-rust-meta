19fb0b9ea5f000cdbffa7921b00359ffb647a412
Kent Fredric <kentnl@gentoo.org>
2019-12-20 02:45:43 +1300

    crc32fast 1.2.0 enable tests

--- meta/c/crc32fast/1.2.0.pl
+++ meta/c/crc32fast/1.2.0.pl
@@ -3,13 +3,13 @@ use strict;
 use warnings;
 
 crate(
-    crc32fast     => '1.2.0',
-    missing_tests => 1,
-    requires      => {
+    crc32fast => '1.2.0',
+    requires  => {
         'cfg-if' => '^0.1',
     },
     test => {
-        bencher    => '^0.1',
+
+        # bencher    => '^0.1', # benchmark, patched
         quickcheck => '^0.6',
         rand       => '^0.4',
     },
