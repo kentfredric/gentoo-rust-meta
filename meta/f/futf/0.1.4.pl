812551d29ca1b06ca74958c8f540243592ee0340
Kent Fredric <kentnl@gentoo.org>
2020-01-01 23:35:54 +1300

    +futf 0.1.4

---
+++ meta/f/futf/0.1.4.pl
@@ -0,0 +1,11 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    futf     => '0.1.4',
+    requires => {
+        mac                   => '^0.1.0',
+        new_debug_unreachable => '^1.0.0',
+    }
+);
