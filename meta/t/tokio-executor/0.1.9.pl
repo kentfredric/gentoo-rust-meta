75cbb44f5415d0a6ac15ef34badb8a5527bf5365
Kent Fredric <kentnl@gentoo.org>
2020-01-04 00:14:45 +1300

    tokio-executor 0.1.9 enable tests

--- meta/t/tokio-executor/0.1.9.pl
+++ meta/t/tokio-executor/0.1.9.pl
@@ -4,7 +4,6 @@ use warnings;
 
 crate(
     'tokio-executor' => '0.1.9',
-    missing_tests    => 1,
     requires         => {
         'crossbeam-utils' => '^0.6.2',
         futures           => '^0.1.19',
