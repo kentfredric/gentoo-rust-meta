2f9319f1b3900dc8398ceade8feaee3a9b13fedc
Kent Fredric <kentnl@gentoo.org>
2020-01-06 22:28:09 +1300

    +proc-macro2 1.0.7

---
+++ meta/p/proc-macro2/1.0.7.pl
@@ -0,0 +1,19 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    'proc-macro2' => '1.0.7',
+    requires      => {
+        'unicode-xid' => '^0.2',
+    },
+    test => {
+        quote => '^1.0',
+    },
+    features => {
+        default          => ['proc-macro'],
+        nightly          => [],
+        'proc-macro'     => [],
+        'span-locations' => [],
+    }
+);
