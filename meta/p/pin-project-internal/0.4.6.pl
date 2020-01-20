85eeca1585b189b8b72ffd7b7a3dbf67a5b328bd
Kent Fredric <kentnl@gentoo.org>
2020-01-20 23:34:02 +1300

    +pin-project-internal 0.4.6

---
+++ meta/p/pin-project-internal/0.4.6.pl
@@ -0,0 +1,12 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    'pin-project-internal' => '0.4.6',
+    requires               => {
+        'proc-macro2' => '^1.0',
+        quote         => '^1.0',
+        syn           => '^1.0',
+    },
+);
