2ff8633a89aa51740eb1c0e3cef301cd338886dd
Kent Fredric <kentnl@gentoo.org>
2020-01-22 00:41:12 +1300

    +futures-join-macro-preview 0.3.0-alpha.19

---
+++ meta/f/futures-join-macro-preview/0.3.0-alpha.19.pl
@@ -0,0 +1,14 @@
+#!perl
+use strict;
+use warnings;
+
+# 0.3.0-alpha.19 / 0.3.0_alpha19
+crate(
+    'futures-join-macro-preview' => '0.3.0.19',
+    requires                     => {
+        'proc-macro-hack' => '^0.5.9',
+        'proc-macro2'     => '^1',
+        quote             => '^1',
+        syn               => '^1',
+    },
+);
