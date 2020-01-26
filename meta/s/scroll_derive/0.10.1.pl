f1e310946e5069b29b680ab094628ec28a5bd9d3
Kent Fredric <kentnl@gentoo.org>
2020-01-26 20:56:57 +1300

    +scroll_derive 0.10.1

---
+++ meta/s/scroll_derive/0.10.1.pl
@@ -0,0 +1,20 @@
+#!perl
+use strict;
+use warnings;
+
+# Patched in
+my %missing = (
+    test => {
+        'scroll' => '^0.10',
+    },
+);
+crate(
+    scroll_derive => '0.10.1',
+    missing_tests => 1,
+    requires      => {
+        'proc-macro2' => '^1',
+        quote         => '^1',
+        syn           => '^1',
+    },
+    test => { %{ $missing{test} }, },
+);
