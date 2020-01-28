4ba508267bdacd09a892f76ba438b2e3aa74d51a
Kent Fredric <kentnl@gentoo.org>
2020-01-28 14:37:04 +1300

    +goblin 0.1.3

---
+++ meta/g/goblin/0.1.3.pl
@@ -0,0 +1,32 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    goblin   => '0.1.3',
+    optional => {
+        log => '^0.4',
+    },
+    requires => {
+        plain  => '^0.2.3',
+        scroll => '^0.10',
+    },
+    features => {
+        alloc   => [ "scroll/derive", "log" ],
+        archive => ["alloc"],
+        default => [
+            "std",    "elf32", "elf64", "mach32",
+            "mach64", "pe32",  "pe64",  "archive",
+            "endian_fd"
+        ],
+        elf32     => [],
+        elf64     => [],
+        endian_fd => ["alloc"],
+        mach32    => [ "alloc", "endian_fd" ],
+        mach64    => [ "alloc", "endian_fd" ],
+        pe32      => [ "alloc", "endian_fd" ],
+        pe64      => [ "alloc", "endian_fd" ],
+        std       => [ "alloc", "scroll/std" ],
+    }
+);
+
