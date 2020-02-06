f9f777e32aa3b3dc6f74a5f2d0b978b82679f3fd
Kent Fredric <kentnl@gentoo.org>
2020-02-06 17:55:41 +1300

    +siphasher 0.3.1

---
+++ meta/l/lock_api/0.3.3.pl
@@ -0,0 +1,12 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    lock_api => '0.3.3',
+    optional => {
+        'owning_ref' => '^0.4',
+        scopeguard   => '^1',
+        serde        => '^1.0.90',
+    }
+);
---
+++ meta/s/siphasher/0.3.1.pl
@@ -0,0 +1,5 @@
+#!perl
+use strict;
+use warnings;
+
+crate( siphasher => '0.3.1', );
