32473f80e63145a16be52bf426ca4190a68edf50
Kent Fredric <kentnl@gentoo.org>
2020-02-06 19:59:45 +1300

    phf_macros 0.8.0 enable tests

--- meta/p/phf_macros/0.8.0.pl
+++ meta/p/phf_macros/0.8.0.pl
@@ -3,9 +3,8 @@ use strict;
 use warnings;
 
 crate(
-    phf_macros    => '0.8.0',
-    missing_tests => 1,
-    requires      => {
+    phf_macros => '0.8.0',
+    requires   => {
         phf_generator     => '^0.8.0',
         phf_shared        => '^0.8.0',
         'proc-macro-hack' => '^0.5.4',
