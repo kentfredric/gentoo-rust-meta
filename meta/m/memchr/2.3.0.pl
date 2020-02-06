b78e43ee308d07c8d9159a4bd1d56b0dcb202273
Kent Fredric <kentnl@gentoo.org>
2020-02-06 23:07:29 +1300

    +memchr 2.3.0

---
+++ meta/m/memchr/2.3.0.pl
@@ -0,0 +1,18 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    memchr   => '2.3.0',
+    optional => {
+        libc => '^0.2.18',
+    },
+    test => {
+        quickcheck => '^0.9',
+    },
+    features => {
+        default => [qw( std )],
+        std     => [],
+        use_std => [qw( std )]
+    },
+);
