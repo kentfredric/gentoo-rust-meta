cbd8912fa8229d3fa2ba7d9dadf19cdbea95fca5
Kent Fredric <kentnl@gentoo.org>
2020-02-17 00:57:33 +1300

    +ordermap 0.4.2

---
+++ meta/o/ordermap/0.4.2.pl
@@ -0,0 +1,22 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    ordermap => '0.4.2',
+    optional => {
+        serde => '^1',
+    },
+    test => {
+        fnv        => '^1',
+        itertools  => '^0.7.0',
+        quickcheck => '^0.6',
+        serde_test => '^1.0.5',
+    },
+    features => {
+        'serde-1'                 => [qw( serde )],
+        test_debug                => [],
+        test_low_transition_point => [],
+    }
+);
+
