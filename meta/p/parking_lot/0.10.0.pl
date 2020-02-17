43a7c0c039ea4a9204fa1ea0b523b998ab068fa8
Kent Fredric <kentnl@gentoo.org>
2020-02-17 15:54:28 +1300

    +parking_lot 0.10.0

---
+++ meta/p/parking_lot/0.10.0.pl
@@ -0,0 +1,23 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    parking_lot => '0.10.0',
+    requires    => {
+        lock_api         => '^0.3.1',
+        parking_lot_core => '^0.7',
+    },
+    test => {
+        bincode     => '^1.1.3',
+        lazy_static => '^1',
+        rand        => '^0.7',
+    },
+    features => {
+        deadlock_detection => ['parking_lot_core/deadlock_detection'],
+        default            => [],
+        nightly    => [ 'parking_lot_core/nightly', 'lock_api/nightly' ],
+        owning_ref => ['lock_api/owning_ref'],
+        serde      => ['lock_api/serde'],
+    }
+);
