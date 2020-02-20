63ef8b70f12c51a97e4a0eb9ae645ebf8a7ae4ed
Kent Fredric <kentnl@gentoo.org>
2020-02-20 17:15:11 +1300

    +afl 0.6.0

---
+++ meta/a/afl/0.6.0.pl
@@ -0,0 +1,25 @@
+#!perl
+use strict;
+use warnings;
+
+my %bin_cargo_afl = (
+    cc   => '^1.0',
+    clap => '^2.29',
+);
+crate(
+    afl      => '0.6.0',
+    requires => {
+        rustc_version => '^0.2',
+        xdg           => '^2.1',
+    },
+    optional       => {%bin_cargo_afl},
+    build_requires => {
+
+        #  rustc_version => '^0.2',
+        #  xdg           => '^2.1',
+    },
+    features => {
+        'f_cargo-afl' => [qw( cc clap )],
+    }
+);
+
