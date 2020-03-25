f9da410ca5af351bb46e35796daa67bcd52dcb15
Kent Fredric <kentnl@gentoo.org>
2020-03-26 01:58:15 +1300

    +signal-hook 0.1.13

---
+++ meta/s/signal-hook/0.1.13.pl
@@ -0,0 +1,24 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    'signal-hook' => '0.1.13',
+    optional      => {
+        futures         => '~0.1',
+        mio             => '~0.6',
+        'tokio-reactor' => '~0.1',
+    },
+    requires => {
+        libc                   => '~0.2',
+        'signal-hook-registry' => '~1.2',
+    },
+    test => {
+        tokio          => '~0.1',
+        'version-sync' => '~0.8',
+    },
+    features => {
+        'mio-support'   => [qw( mio )],
+        'tokio-support' => [qw( futures mio-support tokio-reactor )],
+    }
+);
