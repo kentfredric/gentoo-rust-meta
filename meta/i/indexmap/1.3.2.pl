dbe5d35e8c757f74feb5d9e5e99acdf30cbed741
Kent Fredric <kentnl@gentoo.org>
2020-03-26 21:52:46 +1300

    +indexmap 1.3.2

---
+++ meta/i/indexmap/1.3.2.pl
@@ -0,0 +1,27 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    indexmap       => '1.3.2',
+    build_requires => {
+        autocfg => '^1',
+    },
+    optional => {
+        rayon => '^1.0',
+        serde => '^1.0',
+    },
+    test => {
+        fnv         => '^1.0',
+        itertools   => '^0.8',
+        lazy_static => '^1.3',
+        quickcheck  => '^0.8',
+        rand        => '^0.6',
+        serde_test  => '^1.0.99',
+    },
+    features => {
+        'serde-1'                 => ['serde'],
+        test_debug                => [],
+        test_low_transition_point => [],
+    }
+);
