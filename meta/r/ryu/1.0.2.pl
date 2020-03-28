c7a44e4426d60f40fca981cbb3867a68f570187a
Kent Fredric <kentnl@gentoo.org>
2020-03-29 05:29:22 +1300

    +ryu 1.0.2

---
+++ meta/r/ryu/1.0.2.pl
@@ -0,0 +1,18 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    ryu      => '1.0.2',
+    optional => {
+        'no-panic' => '^0.1',
+    },
+    test => {
+        num_cpus => '^1.8',
+        rand     => '^0.5',
+    },
+    features => {
+        'small' => [],
+    },
+);
+
