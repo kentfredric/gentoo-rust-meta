885594b11c76a5557eba78f18907eec856c6e81a
Kent Fredric <kentnl@gentoo.org>
2020-03-29 02:50:24 +1300

    +cpp_demangle 0.2.14

---
+++ meta/c/cpp_demangle/0.2.14.pl
@@ -0,0 +1,32 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    cpp_demangle => '0.2.14',
+    optional     => {
+        afl => '^0.5.1',
+    },
+    requires => {
+        'cfg-if' => '^0.1.9',
+    },
+    test => {
+
+        # clap => '^2.33.0', # example
+        diff => '^0.1.11',
+    },
+    build_requires => {
+        glob => '^0.3.0',
+    },
+    features => {
+        alloc               => [],
+        cppfilt             => [],
+        default             => [qw( std )],
+        fuzz                => [qw( afl )],
+        logging             => [],
+        nightly             => [],
+        run_libiberty_tests => [],
+        std                 => [],
+    },
+);
+
