0611c6461d52e64b7baf01a078bdc7e29c715f6f
Kent Fredric <kentnl@gentoo.org>
2020-03-29 01:52:32 +1300

    +tokio-current-thread 0.1.7

---
+++ meta/t/tokio-current-thread/0.1.7.pl
@@ -0,0 +1,12 @@
+#!perl
+use strict;
+use warnings;
+
+crate(
+    'tokio-current-thread' => '0.1.7',
+    requires               => {
+        futures          => '^0.1.19',
+        'tokio-executor' => '^0.1.7',
+    }
+);
+
